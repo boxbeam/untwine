7470d0a0651d5981873a0e5609eb3be578cae3ba
boxbeam <instantiable@gmail.com>
2024-01-24 19:51:13 -0500

    Make demo show more syntax

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -4,7 +4,7 @@ struct MyContext {}
 
 parser! {
     [ctx: MyContext]
-    digit: "a"+ ['0'-'9']+ -> char {
+    digit: "a"+ ['0'-'9']+ ("true" | "false") (a b c d) -> char {
         digit
     }
 }
