978a3c59802e16c7a8eb36adfbf1cfe66d2fab4a
boxbeam <instantiable@gmail.com>
2024-01-26 01:56:08 -0500

    Fix split logic to allow re-splitting if the child split has died

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,7 @@ pub struct Stack<'a, T> {
     len: usize,
     phantom: PhantomData<T>,
     split: Cell<bool>,
+    parent_split: Option<&'a Cell<bool>>,
 }
 
 impl<'a, T> Deref for Stack<'a, T> {
@@ -54,27 +55,42 @@ impl<'a, T> Stack<'a, T> {
                 len: 0,
                 phantom: PhantomData,
                 split: Default::default(),
+                parent_split: None,
             }
         }
     }
 
-    pub fn split<'b, 'c, V>(&'b self) -> Stack<'b, V>
-    where
-        'b: 'a,
-        'c: 'b,
-    {
+    pub fn split<V>(&self) -> Stack<V> {
         if self.split.get() {
             panic!("Stack was split from twice");
         }
         self.split.replace(true);
         let pos = (std::mem::size_of::<T>() * self.len) + std::mem::align_of::<T>();
         let mem = unsafe { (*self.mem).split_at_mut(pos).1 };
-        Stack::new(mem as *mut _)
+        let mut stack = Stack::new(mem as *mut _);
+        stack.parent_split = Some(&self.split);
+        stack
     }
 
     pub fn push(&mut self, val: T) {
         self.int[self.len] = val;
         self.len += 1;
+    }
+}
+
+impl<'a, T> Drop for Stack<'a, T> {
+    fn drop(&mut self) {
+        for t in &mut *self.int {
+            unsafe {
+                drop(std::mem::replace(
+                    t,
+                    std::mem::MaybeUninit::uninit().assume_init(),
+                ));
+            }
+        }
+        if let Some(parent_split) = self.parent_split {
+            parent_split.replace(false);
+        }
     }
 }
 
@@ -99,7 +115,20 @@ mod tests {
     fn test_multi_split() {
         let mut stack = UniversalStack::new(1000);
         let first = stack.stack::<i32>();
-        first.split::<i32>();
+        let mut second = first.split::<i32>();
+        let mut third = first.split::<i32>();
+        second.push(1);
+        third.push(1);
+    }
+
+    #[test]
+    fn test_multi_split_scope() {
+        let mut stack = UniversalStack::new(1000);
+        let first = stack.stack::<i32>();
+        {
+            let mut split = first.split::<i32>();
+            split.push(1);
+        }
         first.split::<i32>();
     }
 }
