1bea879fad5fb2a360ab38e0198decec382ac3a7
boxbeam <instantiable@gmail.com>
2024-01-25 20:36:09 -0500

    These were simpler than I realized

--- src/type_utils.rs
+++ src/type_utils.rs
@@ -1,9 +1,24 @@
-use syn::Type;
+use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
+use quote::quote;
+use syn::{
+    punctuated::Punctuated,
+    token::{Gt, Lt},
+    AngleBracketedGenericArguments, Path, PathArguments, PathSegment, Type, TypePath,
+};
 
-fn optional(typ: Type) -> Type {
-    todo!()
+pub fn optional(typ: Type) -> Type {
+    let tokens: TokenStream = quote! {
+        Option<#typ>
+    }
+    .into();
+    syn::parse(tokens).unwrap()
 }
 
-fn vec_of(typ: Type) -> Type {
-    todo!()
+pub fn vec_of(typ: Type) -> Type {
+    let tokens: TokenStream = quote! {
+        Vec<#typ>
+    }
+    .into();
+    syn::parse(tokens).unwrap()
 }
